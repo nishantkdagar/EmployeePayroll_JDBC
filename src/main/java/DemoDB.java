c505cf52559c8afb2a6d68285b4f77de3263fb68
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-28 23:03:03 +0530

    Connecting to Database

--- src/main/java/DemoDB.java
+++ src/main/java/DemoDB.java
@@ -1,5 +1,39 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class DemoDB {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String username = "root";
+        String password = "password";
+        Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in class path: "+e);
+        }
+        listDrivers();
+
+        try {
+            System.out.println("Connecting to Database: "+jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connection is Successful" +con);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+
 }
